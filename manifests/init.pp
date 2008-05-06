572e06304dea7d22321183d0f21b625ec3d50908
Marcel Haerry <haerry@puzzle.ch>
2008-05-06 10:32:46 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# cron-delay module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "cron-delay": }
+class cron-delay {}
