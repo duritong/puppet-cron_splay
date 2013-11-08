e553a88a22e0d03bc9e3a2b2ac850f1d8ab2f218
mh <mh@immerda.ch>
2013-11-08 16:39:26 +0100

    make it also work on debian

--- files/cron-splay.sh
+++ files/cron-splay.sh
@@ -6,8 +6,6 @@ let "minutes %= 360"
 for type in daily weekly monthly; do
     hour="`expr $minutes / 60`"
     minute="`expr $minutes % 60`"
-#   echo "s@^([0-9]{1,2}) [^\*]{1,2} (.*cron\.${type})@$minute $hour \2@"
-    sed -ri "s@^([0-9]{1,2}) [^\*]{1,2} (.*cron\.${type})@$minute $hour \2@" /etc/crontab
+    sed -ri "s@^([0-9]{1,2}) [^\*]{1,2}([ \t].*cron\.${type})@$minute $hour\2@" /etc/crontab
     minutes="`expr $minutes + 20`"
 done
-
